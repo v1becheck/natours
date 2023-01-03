1420be10f82ff9b0e072a65171977e742789e2a3
Milos Ilic <milosilic89@outlook.com>
2023-01-03 02:08:17 +0100

    Added tours route handler

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server side' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const PORT = 3000;
