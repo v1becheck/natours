9d22373543d30c1846a55e459a3d9a6d5c50146d
Milos Ilic <milosilic89@outlook.com>
2023-01-03 12:19:42 +0100

    Handling PATCH Request

--- app.js
+++ app.js
@@ -59,6 +59,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour here>',
+    },
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
