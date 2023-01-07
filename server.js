07fbb7e800249ea7877eabdfe506b41f3bf955ce
Milos Ilic <milosilic89@outlook.com>
2023-01-07 05:56:00 +0100

    Created Tour Schema and Model

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    required: [true, 'A tour must have a name'],
+  },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
