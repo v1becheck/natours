71393a6242c98d660f6a39930541bda019f09ab8
Milos Ilic <milosilic89@outlook.com>
2023-01-07 05:40:14 +0100

    Created DB connection; Installed Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlPArser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
