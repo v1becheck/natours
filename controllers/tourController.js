022e6378caab3170a2d3a3ef12325a5a5965a285
Milos Ilic <milosilic89@outlook.com>
2023-01-10 17:12:40 +0100

    Implemented Delete Document frm DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -84,9 +84,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    // if (req.params.id) throw Error();
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
