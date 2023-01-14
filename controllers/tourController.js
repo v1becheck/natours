91b079413f08b0934317ce993376ee9bd79ca280
Milos Ilic <milosilic89@outlook.com>
2023-01-14 17:08:49 +0100

    Implemented Advanced Filtering - gte|gt|lte|lt

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,11 +8,18 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1. Filtering
     const queryObj = { ...req.query }; // hard copy
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2. Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // console.log(req.query);
 
     // const tours = await Tour.find()
     //   .where('duration')
