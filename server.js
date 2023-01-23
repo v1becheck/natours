7290236c50140d255c0291d08d39f336dc295d81
Milos Ilic <milosilic89@outlook.com>
2023-01-23 13:27:30 +0100

    Implemented User Creation

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
+  console.log(err);
   console.log('UNHANDLED EXCEPTION! Shutting down...');
   process.exit(1);
 });
