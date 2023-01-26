1f78f27ffea1ed701d2e8b30ed602a04969233e1
Milos Ilic <milosilic89@outlook.com>
2023-01-26 06:02:42 +0100

    Password Reset Functionality: Reset Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,3 +98,18 @@ exports.restrictTo =
 
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1. Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return next(new AppError('There is no user with that email address', 404));
+
+  // 2. Generate the random token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. Send it back as an email
+});
+
+exports.resetPassword = (req, res, next) => {};
