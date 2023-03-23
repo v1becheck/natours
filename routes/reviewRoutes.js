d67fecc3096591a1eeee2e13049c54f737c1de76
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-23 18:47:13 +0100

    Adding a Nested GET Endpoint

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,10 @@ const authController = require('../controllers/authController');
 // mergeParams allows to use route '/:tourId/reviews' from tourRoutes
 const router = express.Router({ mergeParams: true });
 
+// POST /tours/25j235hkg235h3/reviews
+// GET /tours/25j235hkg235h3/reviews
+// GET /tours/25j235hkg235h3/reviews/151g51g15i5g151k3
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
