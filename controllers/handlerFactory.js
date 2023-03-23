22265b351e62829d258eca19ed75699b83eb497f
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-23 19:35:49 +0100

    Building Handler Factory Function: Delete

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,17 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndDelete(req.params.id);
+    // if (req.params.id) throw Error();
+
+    if (!document) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,7 @@
 const Review = require('../models/reviewModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   //Filter queries with tourId in the route
@@ -34,3 +35,5 @@ exports.createReview = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
