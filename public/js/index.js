43aab0d5d6ba3c4ad1e2edd5f2beb7ea07a9602c
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-20 22:57:13 +0200

    Updating User Password with API

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,14 @@
 // import 'core-js/stable';
 // import 'regenerator-runtime';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 import { displayMap } from './leaflet';
 
 const map = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // Get locations from HTML
 if (map) {
@@ -31,7 +32,34 @@ if (userDataForm) {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'User');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    document.querySelector('.btn--save-password').style['opacity'] = 0.6;
+    document.querySelector('.btn--save-password').style['cursor'] = 'auto';
+    document.querySelector('.btn--save-password').disabled = true;
+
+    const currentPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { currentPassword, password, passwordConfirm },
+      'Password'
+    );
+
+    // Clear fields after password update
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.querySelector('.btn--save-password').style['opacity'] = 1;
+    document.querySelector('.btn--save-password').style['cursor'] = 'pointer';
+    document.querySelector('.btn--save-password').disabled = false;
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
 
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,19 +1,28 @@
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type is either 'Password' or 'User' depending on the form that is submitted
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'Password'
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: '/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url: url,
+      data,
     });
 
-    // Reload to settings page after 1.3 sec
+    // Reload to home page after 1.3 sec
     if (res.data.status === 'success') {
-      showAlert('success', 'User updated successfully!');
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/me');
+      }, 1300);
+    }
+    if (res.data.status === 'success') {
+      showAlert('success', `${type} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
