15244b0f70ec34e6a5328da3f75bad4aa669f602
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-24 21:01:38 +0200

    Rendering a User's Booked Tours

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -47,6 +48,20 @@ exports.getAccount = (req, res) => {
     title: `Your account`,
   });
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // 1. Find all bookings
+  const bookings = await Booking.find({ user: req.user.id });
+
+  // 2. Find tours with the returened IDs
+  const tourIDs = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
+  });
+});
 
 // UPDATE USER DATA WITHOUT API
 exports.updateUserData = catchAsync(async (req, res) => {
--- models/bookingModel.js
+++ models/bookingModel.js
@@ -30,7 +30,15 @@ bookingSchema.pre(/^find/, function (next) {
     path: 'tour',
     select: 'name',
   });
+  next();
 });
+
+// Virtualy populate reviews to Tours Model from Tour Model
+// bookingSchema.virtual('tours', {
+//   ref: 'Booking',
+//   localField: '_id',
+//   foreignField: 'tour',
+// });
 
 const Booking = mongoose.model('Booking', bookingSchema);
 
