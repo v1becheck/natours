789d7ca8400edba06523e6207e43b85bc2847b24
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-24 13:51:18 +0200

    Processing Payments on the Front-End

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,14 @@
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { displayMap } from './leaflet';
+import { bookTour } from './stripe';
 
 const map = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // Get locations from HTML
 if (map) {
@@ -78,3 +80,10 @@ if (userPasswordForm) {
 }
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (bookBtn)
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing...';
+    const tourId = e.target.dataset.tourId;
+    bookTour(tourId);
+  });
