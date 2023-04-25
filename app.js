7b243ffbd35645a829058e56be1d9d2990c98bcc
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-25 18:27:39 +0200

    Responding to a SIGTERM Signal; Implementing CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARE
+
+// Implement CORS
+app.use(cors());
+
+// Implement CORS for all methods (patch, delete...)
+app.options('*', cors());
+
+// Use if front-end is on another domain
+// app.use(
+//   cors({
+//     origin: 'https://www.1um1n0517101.com',
+//   })
+// );
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
