0b59d441216e190c960c1c12d43aab35e02d7889
Milos Ilic <milosilic89@outlook.com>
2024-05-26 20:05:36 +0200

    fix: config for mongo

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+  '<USERNAME>',
+  process.env.USERNAME
+).replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
   // .connect(process.env.DATABASE_LOCAL, {
